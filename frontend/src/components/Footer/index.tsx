85c87b67ec92eba3a6de7c203a42600c1f87cb04
Achmad Musyaffa Taufiqi <amusfq@gmail.com>
2022-07-14 15:33:37 +0700

    Update footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,7 +5,8 @@ export default function Footer({}: Props) {
     <div className="bg-blue-500 px-24">
       <div className="text-white h-16 items-center flex justify-center">
         <p className="text-center">
-          Copyright &copy; {new Date().getFullYear()} - SDN 1 Pacet
+          Copyright &copy; {new Date().getFullYear()} - SDN 1 Pacet x{" "}
+          <a href="https://narotama.ac.id/">Universitas Narotama</a>
         </p>
       </div>
     </div>
